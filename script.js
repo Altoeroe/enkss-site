a90aa434704d5d347cdf1b8afb8de6d599242b7b
Altoeroe <shylendran19@gmail.com>
2025-09-11 12:22:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// Automatically load images from the "images" folder
+// NOTE: GitHub Pages cannot list files dynamically
+// So you need to manually add image names here or generate via backend
+const products = [
+  "motor1.jpg",
+  "motor2.jpg",
+  "motor3.jpg"
+];
+
+const container = document.getElementById("product-list");
+
+products.forEach(img => {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `
+    <img src="images/${img}" alt="${img}">
+    <h3>${img.split('.')[0]}</h3>
+  `;
+
+  card.addEventListener("click", () => {
+    window.location.href = `product.html?product=${img}`;
+  });
+
+  container.appendChild(card);
+});
